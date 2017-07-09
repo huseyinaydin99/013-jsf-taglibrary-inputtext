b1fb19336309fd046725f001d44e0da7b5f0a7dc
huso51 <huseyinaydin99@gmail.com>
2017-07-09 21:17:25 +0300

    Add files via upload

---
+++ 013_JSF_TagLibrary_InputText/src/java/com/huseyinaydin/YonetimliBean.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.huseyinaydin;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author husey
+ */
+@ManagedBean
+@RequestScoped
+public class YonetimliBean implements Serializable{
+
+    /**
+     * Creates a new instance of YonetimliBean
+     */
+    private String ad;
+    private String soyad;
+    public YonetimliBean() {
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+    
+}
